e03004cab2a4b0b1ba1e086abbee4e5ef233e6a5
Baluev.mxntage <Baluev.mxntage@ART-BALUEV>
2021-11-30 04:36:20 +0300

    Add project files.

---
+++ 12/12.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+    int n;
+    setlocale(0, "");
+    cout << "Введите размер - ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j || i == n - j + 1){
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
